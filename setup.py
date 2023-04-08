cb32953e7dfc1b862cd11fabb94404fbdcafc51c
simoncelinder <33983037+simoncelinder@users.noreply.github.com>
2023-04-08 11:48:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='ea-giving-optimizer',
     version='0.0.1',
     description='Tool for running scenarios of giving over lifetime',
-    url='https://github.com/Simpa87/ea-giving-optimizer',
+    url='https://github.com/simoncelinder/ea-giving-optimizer',
     author='Simon Celinder',
     author_email='simon.mindfulprofessionals@gmail.com',
     license='None',
